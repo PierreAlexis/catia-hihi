a97a8b49c41fc5edf5599cce15c5252e43767c44
Shiroi Bōshi <133114005+PierreAlexis@users.noreply.github.com>
2026-01-29 16:07:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Tu es sûre?",
+    "Vraiment sûre??",
+    "Haleineuse va",
+    "Allez bb stp...",
+    "Trouduc",
+    "Si tu dis non, Je vais être triste...",
+    "OH DIS OUI CARALHO",
+    "Puta merda....",
+    "Bon ok, je te demande plus...",
+    "Je rigole, dis oui wesh ❤️"
 ];
 
 let messageIndex = 0;
